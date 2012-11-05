d6c9a8dde22be0c21f10f75c700c2943cc5ad0e4
Guoliang Cao <gcao99@gmail.com>
2012-11-04 19:56:24 -0500

    test.js fixture

---
+++ spec/fixtures/test.js
@@ -0,0 +1,15 @@
+["html",
+  ["head",
+    ["title", "Test Title"]
+  ],
+  ["body",
+    "<!-- This is a comment -->",
+    ["div",
+      {
+        "class": "blue-theme",
+        "id": "main"
+      },
+      "Some text"
+    ]
+  ]
+]
